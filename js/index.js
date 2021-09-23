7007cf656d8004c7ce439fd04b66fd36a0fe8a8e
matthe68 <matthewbchristensen@comcast.net>
2021-09-22 21:10:02 -0600

    made changes suggested by the proffessor

---
+++ js/index.js
@@ -0,0 +1,5 @@
+window.onload = function() {
+    const time = "Last Updated: " + String(document.lastModified);
+    var lastUpdated = document.querySelector("#last-updated");
+    lastUpdated.innerHTML = time;
+}
