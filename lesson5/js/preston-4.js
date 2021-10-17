5f0ba872f978579078a8cf82a77bf66558d6ab50
matthe68 <matthewbchristensen@comcast.net>
2021-10-16 20:37:18 -0600

    set up lesson 5 folder

---
+++ lesson5/js/preston-4.js
@@ -0,0 +1,14 @@
+window.onload = function() {
+  let dayNames = ["Sunday","Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+  let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ]
+  let today =new Date();
+  let dayName = dayNames[today.getDay()];
+  let monthName = months[today.getMonth()];
+  let formattedDate = dayName + ", " + today.getDate() + " " + monthName + " "  + today.getFullYear();
+
+  let todayTag = document.querySelector("#today");
+  todayTag.innerHTML = formattedDate;
+}
+function toggleMenu() {
+  document.getElementById("primary-nav").classList.toggle("hide");
+}
