fda06f2bd72affa56a58a42d492a877d02648b02
matthe68 <matthewbchristensen@comcast.net>
2021-10-20 20:57:41 -0600

    mostly done

--- lesson5/js/preston-5.js
+++ lesson5/js/preston-5.js
@@ -6,9 +6,21 @@ window.onload = function() {
   let monthName = months[today.getMonth()];
   let formattedDate = dayName + ", " + today.getDate() + " " + monthName + " "  + today.getFullYear();
 
+  const menuItem = document.querySelector(".navElement");
   let todayTag = document.querySelector("#today");
   todayTag.innerHTML = formattedDate;
+  menuItem.addEventListener('click', event => {
+    var ul = document.getElementById("primary-nav");
+    var items = ul.getElementsByTagName("li");
+    for (var i = 0; i < items.length; ++i) {
+      items[i].classList.remove("active");
+      console.log("no longer active"); 
+    }
+    menuItem.classList.add('active');
+  })
 }
+
 function toggleMenu() {
   document.getElementById("primary-nav").classList.toggle("hide");
+  console.log("toggle");
 }
