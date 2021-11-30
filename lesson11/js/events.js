2fce88f8139fd4fdda3f113f75fc229b0f41f4c2
matthe68 <matthewbchristensen@comcast.net>
2021-11-29 22:14:49 -0700

    events are dynamically added and styling is finished. fixed placement of events to be lower on the page to make page flow more naturally

--- lesson11/js/events.js
+++ lesson11/js/events.js
@@ -6,6 +6,7 @@ fetch(requestURL)
   })
   .then(function (jsonObject) {
     let towns = jsonObject['towns'];
+    console.log("towns: ", towns);
     let thispage = window.location.href;
     let elements = document.querySelectorAll(".navElement");
     [].forEach.call(elements, function(el) {
@@ -14,6 +15,19 @@ fetch(requestURL)
         for (let i = 0; i < towns.length; i++ ) {
           if (el.firstChild.innerHTML == towns[i].name) {
             console.log("CORRECT PAGE FOUND! BRING IN EVENTS!");
+            let header = document.createElement('h1');
+            let eventsHolder = document.getElementById('upcoming-events');
+            header.innerHTML = "Upcoming Events";
+            header.classList.add('first-header');
+            eventsHolder.appendChild(header);
+
+            eventsArray = towns[i].events;
+            for (eachEvent in eventsArray) {
+              let event = document.createElement('p');
+              event.innerHTML = eventsArray[eachEvent];
+              eventsHolder.appendChild(event);
+              console.log("event added!!!!");
+            }
           }
         }
       }
